a366682307fe9383ac826ebd23d35e4dddb059f2
luis l <luisjoselaresbarrera@gmail.com>
2026-04-02 19:48:55 -0400

    Update script.py

--- script.py
+++ script.py
@@ -1,12 +1,1 @@
 # script_temporal.py
-from core.database import db
-
-with db.get_connection() as conn:
-    conn.execute('''
-        UPDATE settings 
-        SET insta_user = 'ldownloader8@gmail.com',
-            insta_pass = '26718867Luis',
-            groq_key = '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'
-    ''')
-    conn.commit()
-print("Datos cargados.")
